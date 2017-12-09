4be57c2d0a2e4360849e06800ed222c27d0802ef
Adrien Meyer <reyemneirda@gmail.com>
2017-12-09 21:02:27 +0200

    Created the Todo Class model

---
+++ TodoList/TodoList/Models/Todo.swift
@@ -0,0 +1,27 @@
+//
+//  Todo.swift
+//  TodoList
+//
+//  Created by Adrien Meyer on 09/12/2017.
+//  Copyright © 2017 Developer.Institute. All rights reserved.
+//
+
+import Foundation
+
+class ToDo {
+    
+    var title: String
+    var isComplete: Bool
+    var dueDate: Date
+    var notes: String?
+    
+    init(title: String, isComplete: Bool, dueDate: Date, notes: String?) {
+        self.title = title
+        self.isComplete =  isComplete
+        self.dueDate = dueDate
+        self.notes = notes
+    }
+    
+    
+    
+}
